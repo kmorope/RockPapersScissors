89ccae223b886165180d6617c45a5b5e21639c5b
Camilo Romero <kmorope@gmail.com>
2017-03-18 08:44:19 -0500

    The Code Commit
    :sparkles: Added Game Controller
    :sparkles: Added Card and Player Objects
    :sparkles: Added Game Logic to Deal Carts

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card{
+
+    private CardsType _tipoCarta;
+
+    public Card(CardsType tipo){
+        this._tipoCarta = tipo;
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private Player _jugador = new Player();
+
+	private Player _npc = new Player();
+
+	private List<Card> _cartas = new List<Card>();  
+ 
+	void Start()
+	{ 
+		RepartirCartas(_jugador);
+		RepartirCartas(_npc);
+	}
+
+	public void RepartirCartas(Player player){
+		LlenarBolsa();
+		var _playerCards = new List<Card>();
+		for (int i = 0; i < 6; i++)
+		{ 
+			int index = Random.Range(0,_cartas.Count);
+			_playerCards.Add(_cartas[index]);
+			_cartas.Remove(_cartas[index]);
+		}		 
+		player._cartas = _playerCards;
+	}
+
+	public void LlenarBolsa(){
+		_cartas.Clear ();
+		for (int i = 0; i < 3; i++)
+		{
+			_cartas.Add(new Card(CardsType.Piedra));
+			_cartas.Add(new Card(CardsType.Papel));
+			_cartas.Add(new Card(CardsType.Tijera));
+		}
+	}
+}
